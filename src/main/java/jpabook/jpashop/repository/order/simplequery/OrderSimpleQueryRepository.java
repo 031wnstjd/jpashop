87e61335daec29f92f703b82fa96bd568fd1cfbc
Junsung <031wnstjd@naver.com>
2023-09-03 12:22:51 +0900

    add new dto and repo class for OrderSimpleQuery

---
+++ src/main/java/jpabook/jpashop/repository/order/simplequery/OrderSimpleQueryRepository.java
@@ -0,0 +1,27 @@
+package jpabook.jpashop.repository.order.simplequery;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * OrderRepository는 순수한 Entity만 조회하도록 만들기 위해 DTO 조회용 OrderSimpleQueryRepository 클래스를 분리
+ */
+
+@Repository
+@RequiredArgsConstructor
+public class OrderSimpleQueryRepository {
+
+    private final EntityManager em;
+
+    public List<OrderSimpleQueryDto> findOrderDtos() {
+        return em.createQuery(
+                        "select new jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto(o.id, m.name, o.orderDate, o.status, d.address)" +
+                                " from Order o" +
+                                " join o.member m" +
+                                " join o.delivery d", OrderSimpleQueryDto.class)
+                .getResultList();
+    }
+}
